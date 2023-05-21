c0e68e64c8a81651ec9c5ece909e5192c95a8c6e
hannashigemitsu <s-shigemitsuh@bsd405.org>
2023-05-20 21:26:28 -0700

    modified project.java

--- project.java
+++ project.java
@@ -1,54 +1,72 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class project {
+    static final int MAX_USERS = 10;
+    static final int NUM_RIDDLES = 3;
+    static final String[] riddles = {
+            "What are two things you can never eat for breakfast?",
+            "The more you take, the more you leave behind, what are they?",
+            "What can you put in a bucket to make it weigh less?"
+    };
+    static final String[] answers = {
+            "lunch and dinner",
+            "footsteps",
+            "a hole"
+    };
+
     public static void main(String[] args) {
-        final int MAX_ITEMS = 50;
-        String[] items = new String[MAX_ITEMS];
-        int itemCount = 0;
+        String[] usernames = new String[MAX_USERS];
+        int[] scores = new int[MAX_USERS];
+        int userCount = 0;
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Enter items (type 'stop' to end):");
+        System.out.print("Enter your username: ");
+        String username = scanner.nextLine();
 
-        while (itemCount < MAX_ITEMS) {
-            String input = scanner.nextLine();
+        int userIndex = findUserIndex(username, usernames, userCount);
+        int score = 0;
 
-            if (input.equalsIgnoreCase("stop")) {
-                break;
+        if (userIndex != -1) {
+            score = scores[userIndex];
+            System.out.println("Welcome back, " + username + "! Your previous score is: " + score);
+        } else {
+            if (userCount == MAX_USERS) {
+                System.out.println("Sorry, maximum number of users reached.");
+                return;
             }
-
-            items[itemCount] = input;
-            itemCount++;
+            userIndex = userCount;
+            usernames[userIndex] = username;
+            userCount++;
+            System.out.println("Hello, " + username + "! Your score is: " + score);
         }
 
-        System.out.println("Enter your activity (coding/sleeping/running/eating/other):");
-        String activity = scanner.nextLine();
+        for (int i = 0; i < NUM_RIDDLES; i++) {
+            System.out.println("Riddle " + (i + 1) + ":");
+            System.out.println(riddles[i]);
 
-        String item = searchItem(items, activity);
-        System.out.println(item);
+            System.out.print("Your answer: ");
+            String answer = scanner.nextLine();
+
+            if (answer.equalsIgnoreCase(answers[i])) {
+                score++;
+                System.out.println("Correct! You earned 1 point.");
+            } else {
+                System.out.println("Wrong answer. Moving on to the next riddle.");
+            }
+        }
+
+        scores[userIndex] = score;
+        System.out.println("Game over. Your final score is: " + score);
     }
 
-    public static String searchItem(String[] items, String activity) {
-        ArrayList<String> itemList = new ArrayList<>(Arrays.asList(items));
-
-        if (activity.equalsIgnoreCase("coding") && itemList.contains("computer")) {
-            return "computer";
-        } else if (activity.equalsIgnoreCase("running") && itemList.contains("shoe")) {
-            return "shoe";
-        } else if (activity.equalsIgnoreCase("sleeping") && itemList.contains("bed")) {
-            return "bed";
-        } else if (activity.equalsIgnoreCase("eating") && (itemList.contains("chicken") || itemList.contains("cow"))) {
-            if (itemList.contains("chicken")) {
-                return "chicken";
-            } else {
-                return "cow";
+    public static int findUserIndex(String username, String[] usernames, int userCount) {
+        for (int i = 0; i < userCount; i++) {
+            if (username.equals(usernames[i])) {
+                return i;
             }
-        } else if (activity.equalsIgnoreCase("other")){
-            return "sorry dawg";
-        } else {
-            return "go away you don't have what you need";
         }
+        return -1;
     }
 }
