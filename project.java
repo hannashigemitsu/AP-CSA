3849c0b3ed4d7a603e9001c342ff059ceffc8435
milamanfredi <134097746+milamanfredi@users.noreply.github.com>
2023-06-06 21:35:51 +0000

    riddles 4-9

--- project.java
+++ project.java
@@ -5,16 +5,29 @@ import java.util.Scanner;
 
 public class project {
     static final int MAX_USERS = 10;
-    static final int NUM_RIDDLES = 3;
+    static final int NUM_RIDDLES = 10;
     static final String[] riddles = {
             "What are two things you can never eat for breakfast?",
             "The more you take, the more you leave behind, what are they?",
-            "What can you put in a bucket to make it weigh less?"
+            "What can you put in a bucket to make it weigh less?",
+            "What does a house wear?",
+            "Where would you take a sick boat?",
+            "What creature is smarter than a talking parrot?",
+            "How does a bee get to school?",
+            "Why are As like flowers?",
+            "What do you a snail on a ship?"
+
     };
     static final String[] answers = {
             "lunch and dinner",
             "footsteps",
-            "a hole"
+            "a hole",
+            "ad-dress",
+            "to the dock",
+            "a spelling bee",
+            "on a buzz",
+            "because Bs come after them",
+            "a snailor"
     };
 
     public static void main(String[] args) {
