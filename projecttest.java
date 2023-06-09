8c2de844cf58193c5616ff208f4e2f039f74eaf0
hannashigemitsu <107134772+hannashigemitsu@users.noreply.github.com>
2023-06-09 05:24:59 +0000

    edit

--- projecttest.java
+++ projecttest.java
@@ -81,77 +81,37 @@ public class projecttest {
     static void drawHeart(int size) {
         switch (size) {
             case 1:
-                System.out.println("  ..*....*..");
-                System.out.println("*. .*******. *.");
-                System.out.println("....*.....*....");
-                System.out.println("......*...*......");
-                System.out.println("........*........");
+                System.out.println("<3");
                 break;
             case 2:
-                System.out.println("   ..**..**..");
-                System.out.println(" .*...........*.");
-                System.out.println(".....*.....*.....");
-                System.out.println(".......*...*.......");
-                System.out.println(".........*.........");
+                System.out.println("<3 <3");
                 break;
             case 3:
-                System.out.println("    ..***.***..");
-                System.out.println("  .*.............*.");
-                System.out.println("......*.......*......");
-                System.out.println("........*...*........");
-                System.out.println("..........*..........");
+                System.out.println("<3 <3 <3");
                 break;
             case 4:
-                System.out.println("     ..****.****..");
-                System.out.println("   .*...............*.");
-                System.out.println(".......*.........*.......");
-                System.out.println("..........*...*..........");
-                System.out.println("............*............");
+                System.out.println("<3 <3 <3 <3");
                 break;
             case 5:
-                System.out.println("      ..*****.*****..");
-                System.out.println("    .*..................*.");
-                System.out.println("........*...........*........");
-                System.out.println("...........*.....*...........");
-                System.out.println("..............*..............");
+                System.out.println("<3 <3 <3 <3 <3");
                 break;
             case 6:
-                System.out.println("       ..******.******..");
-                System.out.println("     .*....................*.");
-                System.out.println("........*.............*.........");
-                System.out.println("............*.......*...........");
-                System.out.println("................*................");
+                System.out.println("<3 <3 <3 <3 <3 <3");
                 break;
             case 7:
-                System.out.println("        ..*******.*.*******..");
-                System.out.println("      .*......................*.");
-                System.out.println("........*...............*........");
-                System.out.println(".............*.....*.............");
-                System.out.println(".................*................");
+                System.out.println("<3 <3 <3 <3 <3 <3 <3");
                 break;
             case 8:
-                System.out.println("         ..********.********..");
-                System.out.println("       .*........................*.");
-                System.out.println("........*.................*.........");
-                System.out.println("................*.......*...........");
-                System.out.println(".....................*................");
+                System.out.println("<3 <3 <3 <3 <3 <3 <3 <3");
                 break;
             case 9:
-                System.out.println("          ..*********.*********..");
-                System.out.println("        .*..........................*.");
-                System.out.println("........*..........................*.");
-                System.out.println("................*........*............");
-                System.out.println(".......................*................");
+                System.out.println("<3 <3 <3 <3 <3 <3 <3 <3 <3");
                 break;
             case 10:
-                System.out.println("           ..**********.**********..");
-                System.out.println("         .*...........................*.");
-                System.out.println("........*.............................*.");
-                System.out.println("................................*........");
-                System.out.println(".................................*........");
+                System.out.println("<3 <3 <3 <3 <3 <3 <3 <3 <3 <3");
                 break;
             default:
-                System.out.println("Invalid size.");
+                System.out.println("</3");
                 break;
         }
     }
